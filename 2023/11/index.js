7d78abecf601c66de62bd91507612a77f14f3ad3
yodi-n <4159093+yogui09@users.noreply.github.com>
2023-12-12 19:18:02 +0100

    feat(2022): day 11

--- 2023/11/index.js
+++ 2023/11/index.js
@@ -1,37 +1,18 @@
 import {readFileSync} from 'fs';
 
-let file = readFileSync('./input.txt').toString().trim();
-file = file.replaceAll(/^(\.*)$/gm, '$1\n$1');
+const file = readFileSync('./input.txt').toString().trim();
+// file = file.replaceAll(/^(\.*)$/gm, '$1\n$1');
 let universe = file.split('\n').map(line => line.split(''));
 const emptyColumns = [];
 
-for (let x = 0; x < universe[0].length; x++) {
-    let isEmpty = true;
-    for (let y = 0; y < universe.length && isEmpty; y++) {
-        if (universe[y][x] !== '.') {
-            isEmpty = false;
-        }
-    }
-    if (isEmpty) {
-        emptyColumns.push(x);
-    }
-}
 
-// universe = fillUniverse(universe, emptyColumns, 1);
-universe = fillUniverse(universe, emptyColumns, 1000000);
 const galaxies = getGalaxies(universe);
-const res1 = galaxies.reduce((sum, fromGalaxy, i) => {
-    return sum + galaxies.slice(i).reduce((galaxyDistance, toGalaxy) => galaxyDistance + getDistance(fromGalaxy, toGalaxy), 0)
+const driftedGalaxies = getDriftedCoordinates(galaxies, 1000000);
+const res = driftedGalaxies.reduce((sum, fromGalaxy, i) => {
+    return sum + driftedGalaxies.slice(i).reduce((galaxyDistance, toGalaxy) => galaxyDistance + getDistance(fromGalaxy, toGalaxy), 0)
 }, 0);
 
-console.log(res1);
-
-function fillUniverse(universe, emptyColumns, size) {
-    return universe
-        .map(row => emptyColumns
-            .reduce((newRow, x, i) => newRow
-                .toSpliced(x + i, 0, '.'.repeat(size)), row))
-}
+console.log(res);
 
 function getGalaxies(universe) {
     return universe.reduce((rowAcc, row, y) => [
@@ -40,14 +21,47 @@ function getGalaxies(universe) {
     ], []);
 }
 
+function getDriftedCoordinates(galaxies, size) {
+    const emptyColumns = getEmptyColumns(universe);
+    const emptyRows = getEmptyRows(universe);
+
+    return galaxies.map(({y, x}) => {
+        const emptyRowsBefore = emptyRows.filter(row => row < y).length;
+        const emptyColumnsBefore = emptyColumns.filter(column => column < x).length;
+        return {
+            x: x + (emptyColumnsBefore * (size - 1)),
+            y: y + (emptyRowsBefore * (size - 1))
+        }
+    });
+}
+
+function getEmptyColumns(universe) {
+    const emptyColumns = [];
+    for (let x = 0; x < universe[0].length; x++) {
+        let isEmpty = true;
+        for (let y = 0; y < universe.length && isEmpty; y++) {
+            if (universe[y][x] !== '.') {
+                isEmpty = false;
+            }
+        }
+        if (isEmpty) {
+            emptyColumns.push(x);
+        }
+    }
+    return emptyColumns;
+}
+
+function getEmptyRows(universe) {
+    return universe.reduce((acc, row, i) => {
+        if (row.every(c => c === '.')) {
+            return [...acc, i];
+        }
+        return acc;
+    }, [])
+}
+
 function getDistance(start, end) {
     const xDif = Math.abs(start.x - end.x);
     const yDif = Math.abs(start.y - end.y);
     return xDif + yDif;
-}
-
-function printUniverse(universe) {
-    universe.forEach(row => {
-        console.log(row.join(''));
-    })
 }
